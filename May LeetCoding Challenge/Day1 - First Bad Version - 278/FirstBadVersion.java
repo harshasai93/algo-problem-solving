f2629372c65ff79cc5e45906f6916531565c6eef
Harsha <harshasai93@gmail.com>
2020-05-03 13:29:43 -0400

    added comments in Java File for FirstBadVersion.java

--- May LeetCoding Challenge/Day1 - First Bad Version - 278/FirstBadVersion.java
+++ May LeetCoding Challenge/Day1 - First Bad Version - 278/FirstBadVersion.java
@@ -1,22 +1,28 @@
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
 
-        // return
+        // first bad version is 1 when only 1 version exists
         if(n == 1) return 1;
+
+        // latest is the bad version
         int firstBad = n;
 
+        // assign l,r variables for left and right boundaries of the search window
+        // for Binary Search
         int l = 1;
         int r = n;
 
         while(l <= r){
-
             int m = l + (r-l)/2;
             boolean isBad = isBadVersion(m);
 
+            // if middle is bad then store the middle in firstBad and continue
+            // search in left space to the middle to find even earlier bad version
             if(isBad){
                 firstBad = m;
                 r = m-1;
             }else{
+                // if middle is not bad search in the right space to the middle
                 l = m+1;
             }
         }
